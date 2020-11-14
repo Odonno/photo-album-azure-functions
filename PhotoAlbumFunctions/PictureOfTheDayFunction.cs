4687dd4160626c58733dc03b5517efd1cc6ecac9
David Bottiau <bottiau.david@laposte.net>
2020-11-14 17:02:47 +0100

    feat: add timer function to get bing picture of the day

---
+++ PhotoAlbumFunctions/PictureOfTheDayFunction.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using PhotoAlbumFunctions.Data;
+using PhotoAlbumFunctions.Helpers;
+
+namespace PhotoAlbumFunctions
+{
+    public class PictureOfTheDayFunction
+    {
+        private readonly PhotoAlbumContext _context;
+
+        public PictureOfTheDayFunction(PhotoAlbumContext context)
+        {
+            _context = context;
+        }
+
+        [FunctionName(nameof(PictureOfTheDayFunction))]
+        public async Task Run([TimerTrigger("0 0 0 * * *")] TimerInfo timer, ILogger log)
+        {
+            string imageUrlOfTheDay = await BingHelper.GetImageOfTheDayUrl();
+
+            var photo = new Photo
+            {
+                Url = imageUrlOfTheDay,
+                CreatedAt = DateTime.Now,
+                Tags = new List<Tag>()
+            };
+
+            _context.Photos.Add(photo);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
